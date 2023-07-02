9362e49ac513144f5f93864a0ee0240faa745e3b
Jack Fink <jfink@its4logistics.com>
2023-07-01 20:58:00 -0400

    captain commands

--- ConvexAuctionBot/Modules/CaptainModule.cs
+++ ConvexAuctionBot/Modules/CaptainModule.cs
@@ -3,13 +3,27 @@ using Discord.Interactions;
 
 namespace ConvexAuctionBot.Modules;
 
+[Group("captain", "commands for managing captains")]
 public class CaptainModule : InteractionModuleBase<SocketInteractionContext>
 {
     public InteractionService Commands { get; set; } = null!;
     public CommandHandler _handler;
-
+    
     public CaptainModule(CommandHandler handler)
     {
         _handler = handler;
+    }
+
+    [SlashCommand("add", "add captain to database with default balance of 500")]
+    public async Task AddCaptain(string name, int balance = 500)
+    {
+        
+    }
+    
+    [SlashCommand("delete", "delete captain from database")]
+
+    public async Task DeleteCaptain(string name)
+    {
+        
     }
 }
