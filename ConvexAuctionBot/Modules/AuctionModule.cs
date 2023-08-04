3d48eaaf9557f9c1746d7b9fd207afa143ad2c92
bugg86 <jackfink68@yahoo.com>
2023-08-03 22:21:31 -0400

    small change testing username config

--- ConvexAuctionBot/Modules/AuctionModule.cs
+++ ConvexAuctionBot/Modules/AuctionModule.cs
@@ -1,4 +1,5 @@
 ﻿using ConvexAuctionBot.Handlers;
+using ConvexAuctionBot.Services.Interfaces;
 using Discord.Interactions;
 
 namespace ConvexAuctionBot.Modules;
@@ -7,15 +8,17 @@ public class AuctionModule : InteractionModuleBase<SocketInteractionContext>
 {
     public InteractionService Commands { get; set; } = null!;
     public CommandHandler _handler;
+    private readonly IAuctionService _auctionService;
 
-    public AuctionModule(CommandHandler handler)
+    public AuctionModule(CommandHandler handler, IAuctionService auctionService)
     {
         _handler = handler;
+        _auctionService = auctionService;
     }
 
     public async Task StartAuction(string player)
     {
-        
+        _auctionService.SetStatus("true");
     }
 
     public async Task StopAuction()
