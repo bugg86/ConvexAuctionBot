fdf8153695042dd03bc7289fa144abb801ec0004
bugg86 <jackfink68@yahoo.com>
2023-10-07 10:13:01 -0400

    allow Bid prefix and added correct channel id

--- ConvexAuctionBot/Handlers/CommandHandler.cs
+++ ConvexAuctionBot/Handlers/CommandHandler.cs
@@ -24,7 +24,7 @@ public class CommandHandler
         _client = client;
         _commands = commands;
         _services = services;
-        auctionChannelId = 1124063514001154098;
+        auctionChannelId = 1160214726500950077;
         //I'm pretty sure this is not how I'm supposed to do this but i dont care
         _auctionService = _services.GetRequiredService<IAuctionService>();
         _captainService = _services.GetRequiredService<ICaptainService>();
